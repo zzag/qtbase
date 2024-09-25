16ed137ef4dee63ee6f6c4af9ca96c4538b9f61c
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2024-09-25 19:32:43 +0200

    QStyle: remove now unneeded macros for QPixmapCache
    Remove BEGIN_STYLE_PIXMAPCACHE and END_STYLE_PIXMAPCACHE as they are no
    longer needed and replaced by QCachedPainter.
    
    Task-number: QTBUG-129680
    Change-Id: I1a98b378c908c1369ca58a64e97b6aafd7897018
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

--- src/widgets/styles/qstyle_p.h
+++ src/widgets/styles/qstyle_p.h
@@ -5,7 +5,6 @@
 #define QSTYLE_P_H
 
 #include "private/qobject_p.h"
-#include <QtGui/qguiapplication.h>
 #include <QtWidgets/qstyle.h>
 
 QT_BEGIN_NAMESPACE
@@ -42,35 +41,6 @@ inline QPixmap styleCachePixmap(const QSize &size, qreal pixelRatio)
     cachePixmap.fill(Qt::transparent);
     return cachePixmap;
 }
-
-#define BEGIN_STYLE_PIXMAPCACHE(a) \
-    QRect rect = option->rect; \
-    QPixmap internalPixmapCache; \
-    QPainter *p = painter; \
-    const auto dpr = QStyleHelper::getDpr(p); \
-    const QString unique = QStyleHelper::uniqueName((a), option, option->rect.size(), dpr); \
-    int txType = painter->deviceTransform().type() | painter->worldTransform().type(); \
-    const bool doPixmapCache = (!option->rect.isEmpty()) \
-            && ((txType <= QTransform::TxTranslate) || (painter->deviceTransform().type() == QTransform::TxScale)); \
-    if (doPixmapCache && QPixmapCache::find(unique, &internalPixmapCache)) { \
-        painter->drawPixmap(option->rect.topLeft(), internalPixmapCache); \
-    } else { \
-        if (doPixmapCache) { \
-            rect.setRect(0, 0, option->rect.width(), option->rect.height()); \
-            internalPixmapCache = styleCachePixmap(option->rect.size(), dpr); \
-            p = new QPainter(&internalPixmapCache); \
-        }
-
-
-
-#define END_STYLE_PIXMAPCACHE \
-        if (doPixmapCache) { \
-            p->end(); \
-            delete p; \
-            painter->drawPixmap(option->rect.topLeft(), internalPixmapCache); \
-            QPixmapCache::insert(unique, internalPixmapCache); \
-        } \
-    }
 
 // small helper to read out the pixmap to paint from QPixmapCache or re-draw
 // it and put it into the QPixmapCache for later usage
