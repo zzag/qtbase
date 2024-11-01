zzag/qtbase

commit d897e6a5f37a8ddd99d6d1cfe2b91e14a3dfdeed

QAnyStringView: fix (char) ctor producing an invalid UTF-8 sequence
While Qt expects const char* to be UTF-8, existing APIs treat a single
char as a L1 character, incl. QChar(char) and QString::arg(char).

Even char8_t literals are only allowed to contain US-ASCII content,
therefore are a subset of L1. Fix the detection of encoding
to treat all 1-byte characters as L1 and the rest as UTF-16.

This restores consistency with overload sets including QChar, and the
intention of the original author. While treating char(0xE4) as a
malformed UTF-8 sequence can theoretically be argued for, that
behavior would be useless in practice.

This change does not affect sliced()/first()/last(), nor front() and
back().

[ChangeLog][QtCore][QAnyStringView] Fixed a bug where a QAnyStringView
constructed from `char` would produce an invalid UTF-8 sequence
instead of a valid Latin-1 one, as expected from the equivalent QChar
constructor.

Fixes: QTBUG-125730
Change-Id: I8cacdd4af4b101db1c2c5797c49fe35a30abdc79
Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/text/qanystringview.h
@@ -153,6 +153,15 @@ private:
         QChar ch;
     };
 
+    template <typename Char>
+    static constexpr QAnyStringView fromCharInternal(const Char &ch) noexcept
+    {
+        if constexpr (sizeof ch == 1) // even char8_t is Latin-1 as single octet
+            return QtPrivate::wrapped_t<Char, QLatin1StringView>{QByteArrayView{&ch, 1}};
+        else // sizeof ch == 2
+            return {&ch, 1};
+    }
+
     explicit constexpr QAnyStringView(const void *d, qsizetype n, std::size_t sizeAndType) noexcept
         : m_data{d}, m_size{std::size_t(n) | (sizeAndType & TypeMask)} {}
 public:
@@ -204,7 +213,7 @@ public:
         : QAnyStringView(capacity = std::forward<Container>(c)) {}
     template <typename Char, if_compatible_char<Char> = true>
     constexpr QAnyStringView(const Char &c) noexcept
-        : QAnyStringView{&c, 1} {}
+        : QAnyStringView{fromCharInternal(c)} {}
     template <typename Char, if_convertible_to<QChar, Char> = true>
     constexpr QAnyStringView(Char ch, QCharContainer &&capacity = QCharContainer()) noexcept
         : QAnyStringView{&(capacity.ch = ch), 1} {}

tests/auto/corelib/text/qanystringview/tst_qanystringview.cpp
@@ -613,10 +613,13 @@ void tst_QAnyStringView::asciiLiteralIsLatin1() const
 
 void tst_QAnyStringView::fromCharacterSpecial() const
 {
-    QEXPECT_FAIL("", "QTBUG-125730", Continue);
     // Treating 'ä' as a UTF-8 sequence doesn't make sense, as it would be
     // invalid. And this is not how legacy Qt APIs handled it, either:
     QCOMPARE_NE(QAnyStringView('\xE4').tag(), QAnyStringView::Tag::Utf8);
+
+    // Latin1 is expected, but UTF-16 is harmless (atm, QL1C is converted to
+    // QChar, thus UTF-16). We only care that it's not UTF-8, anyway:
+    QCOMPARE_NE(QAnyStringView(QLatin1Char('\xE4')).tag(), QAnyStringView::Tag::Utf8);
 }
 
 template <typename StringBuilder>

commit 28eefb03dbe17acd746148ed762f17925396db71

QAnyStringView: fix QASV{u8' '} in C++20 mode
This appears to be a regression caused by
d897e6a5f37a8ddd99d6d1cfe2b91e14a3dfdeed, which didn't take into
account that QBAV (like QBA) don't accept char8_t* arguments.

Instead of trying to cast via QBAV and QL1SV, use the QASV(p, n, tag)
ctor directly.

Task-number: QTBUG-125730
Change-Id: I390e3dd174b73a32bc2abe515e4e0207337659a0
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/text/qanystringview.h
@@ -155,7 +155,7 @@ private:
     static constexpr QAnyStringView fromCharInternal(const Char &ch) noexcept
     {
         if constexpr (sizeof ch == 1) // even char8_t is Latin-1 as single octet
-            return QtPrivate::wrapped_t<Char, QLatin1StringView>{QByteArrayView{&ch, 1}};
+            return QAnyStringView{&ch, 1, size_t{Tag::Latin1}};
         else // sizeof ch == 2
             return {&ch, 1};
     }

tests/auto/corelib/text/qanystringview/tst_qanystringview.cpp
@@ -616,6 +616,7 @@ void tst_QAnyStringView::fromCharacterSpecial() const
     // Treating 'ä' as a UTF-8 sequence doesn't make sense, as it would be
     // invalid. And this is not how legacy Qt APIs handled it, either:
     QCOMPARE_NE(QAnyStringView('\xE4').tag(), QAnyStringView::Tag::Utf8);
+    QCOMPARE_NE(QAnyStringView(u8' ').tag(), QAnyStringView::Tag::Utf8);
 
     // Latin1 is expected, but UTF-16 is harmless (atm, QL1C is converted to
     // QChar, thus UTF-16). We only care that it's not UTF-8, anyway: