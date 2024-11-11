zzag/qtbase

commit b492582b9c4cf679109ed4875aff912e186263cb

freetype: Fix scaling layouts with bitmap fonts
The FreeType font engine would wrongly claim to support scaling
transforms for bitmap fonts, despite ignoring the transform
later. We do support this for color fonts through special casing,
but for other bitmap fonts, we should return false and let the
paint engine handle the scaling instead.

Fixes: QTBUG-111796
Change-Id: I31732b909ef6e0c1afbf01ed4de1f67e994934c3
Reviewed-by: Lars Knoll <lars@knoll.priv.no>
Reviewed-by: Konstantin Ritt <ritt.ks@gmail.com>
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

src/gui/text/freetype/qfontengine_ft.cpp
@@ -190,10 +190,15 @@ int QFreetypeFace::getPointInOutline(glyph_t glyph, int flags, quint32 point, QF
     return Err_Ok;
 }
 
+bool QFreetypeFace::isScalable() const
+{
+    return FT_IS_SCALABLE(face);
+}
+
 bool QFreetypeFace::isScalableBitmap() const
 {
 #ifdef FT_HAS_COLOR
-    return !FT_IS_SCALABLE(face) && FT_HAS_COLOR(face);
+    return !isScalable() && FT_HAS_COLOR(face);
 #else
     return false;
 #endif
@@ -1592,7 +1597,7 @@ void QFontEngineFT::getUnscaledGlyph(glyph_t glyph, QPainterPath *path, glyph_me
 
 bool QFontEngineFT::supportsTransformation(const QTransform &transform) const
 {
-    return transform.type() <= QTransform::TxRotate;
+    return transform.type() <= QTransform::TxRotate && (freetype->isScalable() || freetype->isScalableBitmap());
 }
 
 void QFontEngineFT::addOutlineToPath(qreal x, qreal y, const QGlyphLayout &glyphs, QPainterPath *path, QTextItem::RenderFlags flags)

src/gui/text/freetype/qfontengine_ft_p.h
@@ -77,6 +77,7 @@ public:
 
     int getPointInOutline(glyph_t glyph, int flags, quint32 point, QFixed *xpos, QFixed *ypos, quint32 *nPoints);
 
+    bool isScalable() const;
     bool isScalableBitmap() const;
 
     static void addGlyphToPath(FT_Face face, FT_GlyphSlot g, const QFixedPoint &point, QPainterPath *path, FT_Fixed x_scale, FT_Fixed y_scale);

commit b6247ced7e83af4d51ba20be8068f28cc8c006bc

Freetype: Fix transformed text when subpixel AA is on
6ba003f73295b896aa6dc1fba099daadb4760209 fixed transforming
bitmap fonts for the case where 8 bit antialiasing is used.
This is the case when a transform is set on the painter by
the user, but for some reason it is not when high-dpi is
enabled, triggering QTBUG-111796.

An attempt was made at fixing this by drawing the
untransformed glyphs into the cache and then transforming
them as they were put on the screen. This worked with
Qt Quick, but the raster engine does not support this and
instead falls back to QPainterPath rendering in that case.
So b492582b9c4cf679109ed4875aff912e186263cb caused a
regression and was reverted.

A simpler fix which works for both Qt Quick and QPainter
is to draw the pre-transformed image of the glyph into
the cache for 24-bit AA, same as for 8-bit AA.

In addition, this changes the transformation to nearest
neighbor instead of interpolating, because bitmap fonts
are typically intended to look crisp and it also matches
how native rendered fonts are scaled in Qt Quick.

Pick-to: 6.8
Fixes: QTBUG-111796
Change-Id: I2d9fd9d98d32820e677916c3d1c744331a72175b
Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

src/gui/text/freetype/qfontengine_ft.cpp
@@ -2159,7 +2159,7 @@ QImage QFontEngineFT::alphaMapForGlyph(glyph_t g,
 
     QImage img = alphaMapFromGlyphData(glyph, neededFormat);
     if (needsImageTransform)
-        img = img.transformed(t, Qt::SmoothTransformation);
+        img = img.transformed(t, Qt::FastTransformation);
     else
         img = img.copy();
 
@@ -2176,12 +2176,19 @@ QImage QFontEngineFT::alphaRGBMapForGlyph(glyph_t g,
     if (t.type() > QTransform::TxRotate)
         return QFontEngine::alphaRGBMapForGlyph(g, subPixelPosition, t);
 
+    const bool needsImageTransform = !FT_IS_SCALABLE(freetype->face)
+                                     && t.type() > QTransform::TxTranslate;
+
+
     const GlyphFormat neededFormat = Format_A32;
 
     Glyph *glyph = loadGlyphFor(g, subPixelPosition, neededFormat, t, false, true);
 
     QImage img = alphaMapFromGlyphData(glyph, neededFormat);
-    img = img.copy();
+    if (needsImageTransform)
+        img = img.transformed(t, Qt::FastTransformation);
+    else
+        img = img.copy();
 
     if (!cacheEnabled && glyph != &emptyGlyph)
         delete glyph;