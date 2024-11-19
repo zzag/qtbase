4db368d4741eff4d2b2942c25d5e633648e23a64
Volker Hilsheimer <volker.hilsheimer@qt.io>
2024-11-19 16:24:02 +0100

    QFontEngine: add test coverage and benchmark for all engines
    For some known fonts, confirm that we get the right glyph count and
    glyph index for specific unicode code points.
    
    Create QGuiApplication with a specific font engine, and test the fonts
    with all engines. On Windows, that's DirectWrite, GDI, and Freetype;
    on macOS CoreText and Freetype; and otherwise only Freetype.
    
    Not all fonts will be available with all engines, so test in each test
    function whether the font is a good enough match (family is enough, no
    need to do a deep test).
    
    Add a benchmark as well, using the same setup plumbing, but with
    different test functions.
    
    Change-Id: I2ed279965fc3f1dc3f283d0fe7b018fc3035c67d
    Reviewed-by: Anton Kudryavtsev <antkudr@mail.ru>
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

---
+++ tests/benchmarks/gui/text/qfontengine/tst_qfontengine.cpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#define QFONTENGINE_COMMON
+#include "../../../../auto/gui/text/qfontengine/tst_qfontengine.cpp"
+
+using namespace Qt::StringLiterals;
+
+void tst_QFontEngine::glyphCount()
+{
+    QFETCH(const Candidate, candidate);
+    if (!candidate.isFontAvailable())
+        QSKIP("Font is not available");
+
+    const auto fontEngine = candidate.fontEngine();
+
+    QBENCHMARK {
+        fontEngine->glyphCount();
+    }
+}
+
+void tst_QFontEngine::glyphIndex()
+{
+    QFETCH(const Candidate, candidate);
+    if (!candidate.isFontAvailable())
+        QSKIP("Font is not available");
+
+    const auto fontEngine = candidate.fontEngine();
+
+    QBENCHMARK {
+        fontEngine->glyphIndex(candidate.ucs4);
+    }
+}
